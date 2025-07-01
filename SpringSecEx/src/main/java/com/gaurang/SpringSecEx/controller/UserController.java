65f36257eb9e753262236b2dd463fa8b3c9238b7
gaurang <gaurang.tripathi@nashtechglobal.com>
2025-07-01 08:51:35 +0530

    Added BCrytPasswordEncoder

---
+++ SpringSecEx/src/main/java/com/gaurang/SpringSecEx/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.gaurang.SpringSecEx.controller;
+
+import com.gaurang.SpringSecEx.model.Users;
+import com.gaurang.SpringSecEx.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user)
+    {
+        return service.register(user);
+    }
+}
