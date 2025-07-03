a71529625eddd495925d6d047c1151549f1b74ac
gaurang <gaurang.tripathi@nashtechglobal.com>
2025-07-03 10:08:26 +0530

    Validating Token

--- SpringSecEx/src/main/java/com/gaurang/SpringSecEx/config/SecurityConfig.java
+++ SpringSecEx/src/main/java/com/gaurang/SpringSecEx/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +27,9 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -37,7 +41,8 @@ public class SecurityConfig {
                 .anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults()) //for postman rest api
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .build();
+               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+               .build();
 
 
         //for browser
